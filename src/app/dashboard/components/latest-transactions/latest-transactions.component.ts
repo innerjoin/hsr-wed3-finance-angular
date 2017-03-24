ae2411e72e6ede3c9a5503c9cd982ed8b3ef4e47
Lukas Steiger <lukas.steiger@outlook.com>
2017-03-24 11:35:59 +0100

    read transactions from view-model

--- src/app/dashboard/components/latest-transactions/latest-transactions.component.ts
+++ src/app/dashboard/components/latest-transactions/latest-transactions.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LatestTransactionsComponent implements OnInit {
 
+  transactions = [
+    {"from":"1000002","target":"1000001","amount":5,"total":471,"date":{"$$date":1491163200000},"_id":"00bcfxJCvfkHaphX"},
+    {"from":"1000001","target":"1000002","amount":-80,"total":89,"date":{"$$date":1489932000000},"_id":"00kpD0KZN7FrVG7E"},
+    {"from":"1000002","target":"1000001","amount":31,"total":475,"date":{"$$date":1491854400000},"_id":"01X14rx7JKyCDwY5"}
+  ];
+
   constructor() { }
 
   ngOnInit() {
