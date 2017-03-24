99e936de545c8059fc1fb513fa974622cf1b166f
Lukas Steiger <lukas.steiger@outlook.com>
2017-03-24 15:01:28 +0100

    add routing and component for all transactions view

--- src/app/dashboard/components/index.ts
+++ src/app/dashboard/components/index.ts
@@ -1,3 +1,4 @@
 export * from "./dashboard/dashboard.component";
 export * from "./latest-transactions/latest-transactions.component";
 export * from "./new-payment/new-payment.component";
+export * from "./all-transactions/all-transactions.component";
