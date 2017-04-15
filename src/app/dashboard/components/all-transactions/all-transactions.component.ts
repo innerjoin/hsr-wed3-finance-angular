ba97d65a12356159b7ebd10d16e771c4172645a6
Lukas Steiger <lukas.steiger@outlook.com>
2017-04-15 23:01:37 +0200

    auto-calculate years and months

--- src/app/dashboard/components/all-transactions/all-transactions.component.ts
+++ src/app/dashboard/components/all-transactions/all-transactions.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TransactionTableComponent } from '../transaction-table/transaction-table.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-all-transactions',
@@ -12,22 +13,26 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 export class AllTransactionsComponent implements OnInit {
 
   transactions = [
-    {"from":"1000002","target":"1000001","amount":5,"total":471,"date":{"$$date":1491163200000},"_id":"00bcfxJCvfkHaphX"},
-    {"from":"1000001","target":"1000002","amount":-80,"total":89,"date":{"$$date":1489932000000},"_id":"00kpD0KZN7FrVG7E"},
-    {"from":"1000002","target":"1000001","amount":31,"total":475,"date":{"$$date":1491854400000},"_id":"01X14rx7JKyCDwY5"}
+    {'from':'1000002','target':'1000001','amount':5,'total':471,'date':{'$$date':1491163200000},'_id':'00bcfxJCvfkHaphX'},
+    {'from':'1000001','target':'1000002','amount':-80,'total':89,'date':{'$$date':1489932000000},'_id':'00kpD0KZN7FrVG7E'},
+    {'from':'1000002','target':'1000001','amount':31,'total':475,'date':{'$$date':1491854400000},'_id':'01X14rx7JKyCDwY5'}
   ];
 
-  years = [
-    {value: 2017, viewValue: '2017'},
-    {value: 2016, viewValue: '2016'},
-    {value: 2015, viewValue: '2015'}
-  ];
+  monthSelected = moment().month();
+  months = Array(12).fill(0).map(function(_, idx) {
+    console.log("month: ", _, idx);
+    const monthText = ['Jaunary', 'February', 'March', 'April', 'May', 'June', 
+    'July', 'August', 'September', 'October', 'November', 'December'];
+    return {key: idx, value: idx, text: monthText[idx]};
+  });
+  yearSelected = moment().year();
+  years = Array(3).fill(moment().year()).map(function(maxYear, idx) {
+    const year = maxYear - idx;
+    return {key: year, value: year, text: year};
+  });
 
-  months = [
-    {value: 1, viewValue: 'January'}
-  ];
-
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
