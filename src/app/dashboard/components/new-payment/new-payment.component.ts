ae46e096164ecc37d761a4b936a3176b760efd88
Janic Mikes <janic.mikes@hsr.ch>
2017-04-20 12:05:15 +0200

    add new payment container

--- src/app/dashboard/components/new-payment/new-payment.component.ts
+++ src/app/dashboard/components/new-payment/new-payment.component.ts
@@ -1,14 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {NgModule}      from '@angular/core';
+
+import {FormsModule}   from '@angular/forms';
+
 @Component({
-  selector: 'app-new-payment',
-  templateUrl: './new-payment.component.html',
-  styleUrls: ['./new-payment.component.css']
+    selector: 'app-new-payment',
+    templateUrl: './new-payment.component.html',
+    styleUrls: ['./new-payment.component.css']
 })
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        FormsModule
+    ],
+    declarations: [
+        AppComponent,
+        HeroFormComponent
+    ],
+    bootstrap: [AppComponent]
+})
+
+
 export class NewPaymentComponent implements OnInit {
 
-  constructor() { }
+    paymentStatus = '';
 
-  ngOnInit() {
-  }
+    constructor(public id: number,
+                public name: string,
+                public power: string,
+                public alterEgo?: string) {
+    }
+
+    ngOnInit() {
+    }
+
+    onSubmit() {
+        this.submitted = true;
+    }
+
+    onHandlePayment() {
+        this.paymentStatus = 'You are my hero!';
+    }
 
 }
